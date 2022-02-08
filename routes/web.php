de9cc26de8813e912780ee813cf823234151c140
freek <freek@spatie.be>
2022-02-08 15:04:07 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Spatie\RouteDiscovery\Discovery\Discover;
 
-
 Route::get('/', fn() => view('welcome'));
-
-
-Discover::views()->in(resource_path('views/discovery'));
